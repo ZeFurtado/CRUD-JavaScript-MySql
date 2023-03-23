dcb80cd76fde04b995fd8734ef2899c5752b1a68
ZeFurtado <lukhasfurtado9@gmail.com>
2023-03-22 22:51:55 -0300

    Exibição da Página HTML com CSS funcionando

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
 
-const path = require('path');
 const app = express();
 
-app.get('/', function(request, response){
-    response.sendFile('C:/Users/lukha/source/repos/CRUD-JavaScript-MySql/public/register.html');
-    
-});
-
+app.use(express.static('public'));
 
 app.listen(3000,function(req,res)
 {
     console.log('Server is runin');
 })
+
+
+nome = document.getElementById('nomeInput');
+sobrenome = document.getElementById('sobrenomeInput');
+cpf = document.getElementById('cpfInput');
+sexo = document.getElementById('sexoInput');
+nomeSocial = document.getElementById('nomeSocialInput');
+dataNascimento = document.getElementById('dataNascimentoInput');
