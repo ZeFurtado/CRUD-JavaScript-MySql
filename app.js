9adacc91f1478f400bb4d91d38f52988a5df8dfe
ZeFurtado <lukhasfurtado9@gmail.com>
2023-03-27 16:28:25 -0300

    Criação da pasta public e mudança em alguns nomes de arquivos

--- app.js
+++ app.js
@@ -2,33 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
-
 const app = express();
+var path = require('path');
+
 
 //Template engine
-app.engine("handlebars", handlebars.engine({defaultLayout:'main'}));
+app.engine("handlebars", handlebars.engine({defaultLayout:'cadastro'}));
 app.set('view engine', 'handlebars');
 
-app.use('/css',express.static('css'));
-app.use('/js', express.static('js'));
+app.use(express.static(path.join(__dirname+'/public')));
+
 
 //Routes and Templates
 app.get("/", function(req,res)
 {
-    res.render('index');
+    res.render('telaDeCadastro');
 })
-
-app.get("javascript", function(req, res)
-{
-    res.sendFile(__dirname+"/css/style.css");
-})
-
-
-app.get("javascript", function(req, res)
-{
-    res.sendFile(__dirname+"/js/javascript.js");
-})
-
 
 app.listen(3000,function(req,res)
 {
