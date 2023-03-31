5737c3904ae85412f7a3c88f59ca14d80fae1e13
ZeFurtado <lukhasfurtado9@gmail.com>
2023-03-30 22:26:59 -0300

    Página Home adicionada

--- app.js
+++ app.js
@@ -12,16 +12,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname+'/public')));
 
-
 //Routes and Templates
-app.get("/", function(req,res)
-{
-    res.render('telaDeCadastro');
-})
+app.get("/", function(req,res){res.render('telaInicial')});
+app.get("/telaDeCadastro", function(req, res){res.render('telaDeCadastro')});
 
-app.listen(3000,function(req,res)
-{
-    console.log('Server is runin');
-})
+app.listen(3000,function(req,res){console.log('Server is runin');})
 
 
